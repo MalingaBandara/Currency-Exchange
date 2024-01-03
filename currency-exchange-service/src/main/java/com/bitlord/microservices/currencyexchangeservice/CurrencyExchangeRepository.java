e709d239a56f71c3625b3d7d7f1cf46af5dc2f65
Malinga Bandara <malinga1021@gmail.com>
2024-01-03 09:09:40 +0530

    Proj Init

---
+++ currency-exchange-service/src/main/java/com/bitlord/microservices/currencyexchangeservice/CurrencyExchangeRepository.java
@@ -0,0 +1,10 @@
+package com.bitlord.microservices.currencyexchangeservice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface CurrencyExchangeRepository extends JpaRepository < CurrencyExchange , Long > {
+	
+	CurrencyExchange findByFromAndTo ( String from, String to );
+	
+}
---
+++ currency-exchange-service/src/main/java/com/bitlord/microservices/currencyexchangeservice/CurrencyExchangesController.java
@@ -0,0 +1,45 @@
+package com.bitlord.microservices.currencyexchangeservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class CurrencyExchangesController {
+	
+	
+	@Autowired
+	private CurrencyExchangeRepository repository;
+	
+	
+	@Autowired
+	private Environment environment;
+	
+	
+	@GetMapping( "/currency-exchange/from/{from}/to/{to}" )
+	public CurrencyExchange retriveExchangeValue ( @PathVariable String from, @PathVariable String to ) {
+		
+				
+		CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to); // Jpa Repo Method
+		
+		
+		// null data exception
+		if ( currencyExchange  == null ) {
+			throw new RuntimeException ("Unable to find data for " + from + " to " + to );
+		}
+		
+		
+		String port = environment.getProperty( "local.server.port" ); // get local server port
+		
+		currencyExchange.setEnvironment( port ); // set environment
+		
+		return currencyExchange; 
+		
+		
+	}
+	
+
+}
