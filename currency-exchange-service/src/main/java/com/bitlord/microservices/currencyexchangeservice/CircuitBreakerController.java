18aa4a21f7eac1a826ca836a863b637225ac9916
Malinga Bandara <malinga1021@gmail.com>
2024-01-28 11:50:48 +0530

    set dummy url (this not work) - purpose of brake this

--- currency-exchange-service/src/main/java/com/bitlord/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/bitlord/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -1,7 +1,9 @@
 package com.bitlord.microservices.currencyexchangeservice;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 
 @RestController
@@ -12,9 +14,11 @@ public class CircuitBreakerController {
 	@GetMapping( "/sample-api" )
 	public String sampleApi() {
 		
-		return "Sample API";
+		ResponseEntity<String> forEntity = new RestTemplate().getForEntity( "http://localhost:8080/some-dummy-url", String.class ); // set dummy url (this not work)
+		
+		return forEntity.getBody();
 		
 	}
-	
+		
 
 }
