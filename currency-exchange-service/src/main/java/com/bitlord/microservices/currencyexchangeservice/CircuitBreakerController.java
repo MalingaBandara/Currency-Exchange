d430cb603c442fdd86681ef872610d22f6c430c7
Malinga Bandara <malinga1021@gmail.com>
2024-01-28 12:04:52 +0530

    Add resilience4j retry conig

--- currency-exchange-service/src/main/java/com/bitlord/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/bitlord/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.retry.annotation.Retry;
+
 
 @RestController
 public class CircuitBreakerController {
@@ -12,6 +14,7 @@ public class CircuitBreakerController {
 	
 	
 	@GetMapping( "/sample-api" )
+	@Retry ( name = "default" ) // add retry config 
 	public String sampleApi() {
 		
 		ResponseEntity<String> forEntity = new RestTemplate().getForEntity( "http://localhost:8080/some-dummy-url", String.class ); // set dummy url (this not work)
